a4df4ebdf714b4c111b12649e40121f6b09914a9
Ethan De Jesus <ethanawesome702@gmail.com>
2023-11-16 18:25:28 -0500

    progress

--- vite-project/js/main.js
+++ vite-project/js/main.js
@@ -2,10 +2,25 @@ import '../styles/style.css'
 import {deepwoken} from './mylists'
 
 
-
-
-
 document.querySelector('#app').innerHTML = `
-<h1> hi </h1>`
- 
-console.log(deepwoken)
+<button id="ethanbutton">Button</button>
+<div id="container"> </div>`
+
+
+
+const DOMselectors = {
+    y:document.querySelector("#ethanbutton"),
+    card:document.querySelector(".card"),
+    container:document.querySelector("#container")
+
+}
+
+
+DOMselectors.y.addEventListener("click", function(event){
+    event.preventDefault()
+    var htmlelements = ""
+    let x = deepwoken.filter((something)=>something.attunement==="Frostdraw");
+    x.forEach((sm)=>htmlelements+='<div id="card"> <p>${sm.name}</p></div>')
+    DOMselectors.container.innerHTML = htmlelements
+    
+});
